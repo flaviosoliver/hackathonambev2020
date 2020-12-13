b5aa7bc5c4ffbf506f68b87708862316473d2919
Flavio Oliveira <flavsoliver@gmail.com>
2020-12-13 16:52:09 -0300

    implementação home

---
+++ src/services/data.js
@@ -0,0 +1,15 @@
+const insumos = {
+  lupulo: {
+    america: [ 'Centennial', 'Chinook', 'Columbus' ],
+    inglaterra: [ 'Challenger', 'Fuggle', 'Phoenix' ],
+    alemanha: [ 'Hallertau', 'Magnum', 'Tettnang Tettnanger' ],
+    repTcheca: ['Saaz']
+  },
+  malte: {
+    cevada: [ 'pilsen', 'Pale Ale', 'Caramelo', 'Chocolate', 'Defumados', 'Escuros', 'Pretos e Torrados'],
+    centeio: ['centeio'],
+    trigo: ['trigo'],
+    aveia: ['aveia']
+  },
+  levedura: { altafermentacao, baixafermentacao },
+}
