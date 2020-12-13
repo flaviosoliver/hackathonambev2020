db9d592aa6bfdd2d9de25b51ca6b6f000ef19780
Flavio Oliveira <flavsoliver@gmail.com>
2020-12-13 18:47:03 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,7 @@ export default class Home extends Component {
           Aqui você poderá customizar sua cerveja ao seu jeito, utilizando os melhores insumos, com o padrão de qualidade e segurança profissionais da maior cervejaria do país. <br />
           Escolha os ingredientes, personalize o seu rótulo que resto a gente se encarrega.
         </article>
+        
       </div>
     );
   }
